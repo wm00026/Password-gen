1a08f1976d9dc4dbf95face46eb38cb7ddd462de
William Meredith <wm00026@mix.wvu.edu>
2026-06-29 18:11:59 -0400

    Added comments to a function

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ fn get_input(prompt: &str) -> Result<String, io::Error> {
     Ok(input.trim().to_string())
 }
 
+/// Generates the password for the user
+/// # Arguments
+/// * `chars` - The characters within the password
+/// * `length` - The length of the password
 fn generate_password(chars: &[char], length: usize) -> String {
     let mut rng = rand::rng();
     (0..length)
